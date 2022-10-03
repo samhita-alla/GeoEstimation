e993fb4ea0850becdc8a56992c625dfd9744b930
Samhita Alla <aallasamhita@gmail.com>
2022-10-03 21:51:21 +0530

    revert hyperparams
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- classification/train_base.py
+++ classification/train_base.py
@@ -19,7 +19,8 @@ from classification.dataset import MsgPackIterableDatasetMultiTargetWithDynLabel
 class MultiPartitioningClassifier(pl.LightningModule):
     def __init__(self, hparams: Namespace, build_model: bool = True):
         super().__init__()
-        self.hparams.update(dict(hparams))
+        for key in hparams.keys():
+            self.hparams[key]=hparams[key]
 
         self.partitionings, self.hierarchy = self.__init_partitionings()
         self.set_model_classifier(build_model)
